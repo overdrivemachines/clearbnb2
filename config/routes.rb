3d952de40f3f2b929885264d10a255d8adc2ed92
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-10 18:01:32 -0800

    added gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   root "static_pages#index"
 end
