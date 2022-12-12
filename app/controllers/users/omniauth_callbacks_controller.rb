538ef8f1d05aaaac1a1af1fcf27702f724e949b5
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-11 17:33:25 -0800

    added action google_oauth2 that is called when user signs in using google form

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,6 +8,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
 
+  # called after user has signed in using google's form
+  def google_oauth2
+    user = User.from_omniauth(auth)
+
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] =
+        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
+      redirect_to new_user_session_path
+    end
+  end
+
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
 
@@ -21,10 +36,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
-  # protected
+  protected
 
   # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def after_omniauth_failure_path_for(_scope)
+    # super(scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
+  end
+
+  private
+
+  def auth
+    # grabbing info from request
+    @auth ||= request.env['omniauth.auth']
+  end
 end
