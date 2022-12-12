9902b3523c730a1be94a7a24c77d5c1b6a7e75ba
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-11 17:36:04 -0800

    annotated

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 # == Route Map
 #
-#                   Prefix Verb   URI Pattern                       Controller#Action
-#         new_user_session GET    /users/sign_in(.:format)          devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)          devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)         devise/sessions#destroy
-#        new_user_password GET    /users/password/new(.:format)     devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
-#            user_password PATCH  /users/password(.:format)         devise/passwords#update
-#                          PUT    /users/password(.:format)         devise/passwords#update
-#                          POST   /users/password(.:format)         devise/passwords#create
-# cancel_user_registration GET    /users/cancel(.:format)           devise/registrations#cancel
-#    new_user_registration GET    /users/sign_up(.:format)          devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)             devise/registrations#edit
-#        user_registration PATCH  /users(.:format)                  devise/registrations#update
-#                          PUT    /users(.:format)                  devise/registrations#update
-#                          DELETE /users(.:format)                  devise/registrations#destroy
-#                          POST   /users(.:format)                  devise/registrations#create
-#    new_user_confirmation GET    /users/confirmation/new(.:format) devise/confirmations#new
-#        user_confirmation GET    /users/confirmation(.:format)     devise/confirmations#show
-#                          POST   /users/confirmation(.:format)     devise/confirmations#create
-#          new_user_unlock GET    /users/unlock/new(.:format)       devise/unlocks#new
-#              user_unlock GET    /users/unlock(.:format)           devise/unlocks#show
-#                          POST   /users/unlock(.:format)           devise/unlocks#create
+#                                Prefix Verb     URI Pattern                                  Controller#Action
+#                      new_user_session GET      /users/sign_in(.:format)                     users/sessions#new
+#                          user_session POST     /users/sign_in(.:format)                     users/sessions#create
+#                  destroy_user_session DELETE   /users/sign_out(.:format)                    users/sessions#destroy
+# user_google_oauth2_omniauth_authorize GET|POST /users/auth/google_oauth2(.:format)          users/omniauth_callbacks#passthru
+#  user_google_oauth2_omniauth_callback GET|POST /users/auth/google_oauth2/callback(.:format) users/omniauth_callbacks#google_oauth2
+#                     new_user_password GET      /users/password/new(.:format)                users/passwords#new
+#                    edit_user_password GET      /users/password/edit(.:format)               users/passwords#edit
+#                         user_password PATCH    /users/password(.:format)                    users/passwords#update
+#                                       PUT      /users/password(.:format)                    users/passwords#update
+#                                       POST     /users/password(.:format)                    users/passwords#create
+#              cancel_user_registration GET      /users/cancel(.:format)                      users/registrations#cancel
+#                 new_user_registration GET      /users/sign_up(.:format)                     users/registrations#new
+#                edit_user_registration GET      /users/edit(.:format)                        users/registrations#edit
+#                     user_registration PATCH    /users(.:format)                             users/registrations#update
+#                                       PUT      /users(.:format)                             users/registrations#update
+#                                       DELETE   /users(.:format)                             users/registrations#destroy
+#                                       POST     /users(.:format)                             users/registrations#create
+#                 new_user_confirmation GET      /users/confirmation/new(.:format)            users/confirmations#new
+#                     user_confirmation GET      /users/confirmation(.:format)                users/confirmations#show
+#                                       POST     /users/confirmation(.:format)                users/confirmations#create
+#                       new_user_unlock GET      /users/unlock/new(.:format)                  users/unlocks#new
+#                           user_unlock GET      /users/unlock(.:format)                      users/unlocks#show
+#                                       POST     /users/unlock(.:format)                      users/unlocks#create
 
 Rails.application.routes.draw do
   root "static_pages#index"
