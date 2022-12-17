85d449f587e56a2067d9c2a8a0c8a689018acff8
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-16 21:06:55 -0800

    added initMap callback function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+import "my_script";
+
+// https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
+// the following function executes when script from Places library, Maps JavaScript API is loaded
+// the script is called with parameter callback=initMap to specify the callback function name
+window.initMap = () => {
+  console.log("map initialized");
+};
