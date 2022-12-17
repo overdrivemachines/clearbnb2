126a128ba6c5b164d14601afabaefb67851bb8ae
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-16 21:23:03 -0800

    moved initMap to my_script

---
+++ app/javascript/my_script.js
@@ -0,0 +1,6 @@
+// https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
+// the following function executes when script from Places library, Maps JavaScript API is loaded
+// the script is called with parameter callback=initMap to specify the callback function name
+window.initMap = () => {
+  console.log("map initialized");
+};
