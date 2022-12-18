bf7419ccc4d68c1f31fdfbad6b290648c8665722
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-17 20:00:22 -0800

    updating form with address values from google

--- app/javascript/controllers/address_controller.js
+++ app/javascript/controllers/address_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["input"];
   connect() {
     // this gets executed when there is a html element that has attribute
     // data-controller="address"
@@ -17,9 +18,73 @@ export default class extends Controller {
 
     // let addressField = document.querySelector("#listing_address");
     // console.log(addressField);
+
+    this.autocomplete = new google.maps.places.Autocomplete(this.inputTarget, {
+      componentRestrictions: { country: ["us", "ca"] },
+      fields: ["address_components", "geometry"],
+      types: ["address"],
+    });
+
+    // When the user selects an address from the drop-down, call placeSelected()
+    // which populates the fields in the form like address_line1, address_line2, etc.
+    this.autocomplete.addListener("place_changed", this.placeSelected.bind(this));
   }
 
   placeSelected() {
-    // use values from autocomplete
+    // https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
+    // Get the place details from the autocomplete object.
+    const place = this.autocomplete.getPlace();
+    let address1 = "";
+    let postcode = "";
+    console.log(place);
+
+    // Get each component of the address from the place details,
+    // and then fill-in the corresponding field on the form.
+    // place.address_components are google.maps.GeocoderAddressComponent objects
+    // which are documented at http://goo.gle/3l5i5Mr
+    for (const component of place.address_components) {
+      // string: "street_number" or "route" etc.
+      const componentType = component.types[0];
+
+      switch (componentType) {
+        case "street_number": {
+          address1 = `${component.long_name} ${address1}`;
+          break;
+        }
+
+        case "route": {
+          address1 += component.short_name;
+          break;
+        }
+
+        case "postal_code": {
+          postcode = `${component.long_name}${postcode}`;
+          break;
+        }
+
+        case "postal_code_suffix": {
+          postcode = `${postcode}-${component.long_name}`;
+          break;
+        }
+        case "locality":
+          document.querySelector("#listing_city").value = component.long_name;
+          break;
+        case "administrative_area_level_1": {
+          document.querySelector("#listing_state").value = component.short_name;
+          break;
+        }
+        case "country":
+          document.querySelector("#listing_country").value = component.long_name;
+          break;
+      }
+    }
+
+    // Updating Address Line 1 and Postal Code in the form
+    document.querySelector("#listing_address_line1").value = address1;
+    document.querySelector("#listing_postal_code").value = postcode;
+
+    // Updating Latitude and Longitude in the form
+    document.querySelector("#listing_latitude").value = place.geometry.location.lat();
+    document.querySelector("#listing_longitude").value = place.geometry.location.lng();
   }
 }
