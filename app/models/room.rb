b4267d09c3d5ab6ce348a48a6dec2d4d17ce59a7
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-20 11:57:18 -0800

    added association and made room_type enum

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,10 +4,14 @@
 #
 #  id         :bigint           not null, primary key
 #  listing_id :bigint           not null
-#  room_type  :integer          default(0)
+#  room_type  :integer          default("bedroom")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Room < ApplicationRecord
   belongs_to :listing
+  has_many :beds, dependent: :destroy
+  accepts_nested_attributes_for :beds # creates Room.beds_attributes= method and form.fields_for :beds
+
+  enum room_type: { bedroom: 0, primary_bedroom: 1, living_room: 2, basement: 3 }
 end
