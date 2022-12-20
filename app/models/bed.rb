18962cf13811b29a0d4978c407f14c81cfab06fc
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-20 11:56:38 -0800

    made bed_size enum

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -4,10 +4,11 @@
 #
 #  id         :bigint           not null, primary key
 #  room_id    :bigint           not null
-#  bed_size   :integer          default(0)
+#  bed_size   :integer          default("twin")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Bed < ApplicationRecord
   belongs_to :room
+  enum bed_size: { twin: 0, twin_xl: 1, full: 2, queen: 3, king: 4, california_king: 5 }
 end
