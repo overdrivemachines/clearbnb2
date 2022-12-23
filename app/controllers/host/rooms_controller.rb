0dd68bcf0ae271216c2c5ee886f0e782db834473
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-22 17:16:34 -0800

    setting @rooms and @listing

--- app/controllers/host/rooms_controller.rb
+++ app/controllers/host/rooms_controller.rb
@@ -1,4 +1,12 @@
 class Host::RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_listing
+
   def index
+    @rooms = @listing.rooms.all
+  end
+
+  def set_listing
+    @listing = current_user.listings.find(params[:listing_id])
   end
 end
