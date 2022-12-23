53d2371e296797cc2d6d680f1c6f899ba27dbde2
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-22 19:15:53 -0800

    added create action

--- app/controllers/host/rooms_controller.rb
+++ app/controllers/host/rooms_controller.rb
@@ -2,11 +2,28 @@ class Host::RoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_listing
 
+  # GET host/listings/:listing_id/rooms
   def index
     @rooms = @listing.rooms.all
+    @room = @listing.rooms.build
+  end
+
+  # POST host/listings/:listing_id/rooms
+  def create
+    @room = @listing.rooms.build(room_params)
+
+    flash[:errors] = @room.errors.full_messages if !@room.save
+
+    redirect_to host_listing_rooms_path(@listing)
   end
 
   def set_listing
     @listing = current_user.listings.find(params[:listing_id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:room_type)
   end
 end
