a101f504051804b6ebcf7dca8bf91c9beef0cfc0
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-22 16:01:14 -0800

    sample listings, rooms and beds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.find_by(email: "b@b.com")
+
+10.times do
+  # Create listing
+  listing =
+    user.listings.create(
+      title: Faker::Company.catch_phrase,
+      about: Faker::Quote.jack_handey,
+      max_guests: rand(2...10),
+      address_line1: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      postal_code: Faker::Address.zip_code(state_abbreviation: "CA"),
+      country: "United States",
+      status: :draft,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+    )
+  # Each listing has 1 to 4 rooms
+  rand(1...4).times do
+    # Create room
+    room = listing.rooms.create(room_type: rand(0...3))
+
+    # Each room has 1 to 3 beds
+    rand(1...3).times { room.beds.create(bed_size: rand(0...5)) }
+  end
+end
