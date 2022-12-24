03996c2c3bc2e2c28080bbca3c2a8404eff63c95
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-24 10:12:45 -0800

    added destroy action

--- app/controllers/host/rooms_controller.rb
+++ app/controllers/host/rooms_controller.rb
@@ -10,9 +10,23 @@ class Host::RoomsController < ApplicationController
 
   # POST host/listings/:listing_id/rooms
   def create
+    # bed attributes are nested inside room_params
+    # so this creates new beds in addition to the new room
     @room = @listing.rooms.build(room_params)
 
     flash[:errors] = @room.errors.full_messages if !@room.save
+
+    redirect_to host_listing_rooms_path(@listing)
+  end
+
+  # DELETE host/listings/:listing_id/rooms/:id
+  def destroy
+    room = @listing.rooms.find(params[:id])
+    if room.destroy
+      flash[:notice] = "Room was deleted"
+    else
+      flash[:errors] = "Unable to delete room"
+    end
 
     redirect_to host_listing_rooms_path(@listing)
   end
@@ -24,6 +38,6 @@ class Host::RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:room_type)
+    params.require(:room).permit(:room_type, beds_attributes: [:bed_size])
   end
 end
