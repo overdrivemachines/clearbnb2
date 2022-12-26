c40bad273f1fcc0165828f27b79a9f7858e85243
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-25 17:56:55 -0800

    added 5 random users and 10 listings per user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,47 @@
 
 user = User.find_by(email: "b@b.com")
 
+def create_listings(user)
+  10.times do
+    # Create listing
+    listing =
+      user.listings.create(
+        title: ("House " + Faker::Science.modifier + " " + Faker::Science.element),
+        about: Faker::Quote.jack_handey,
+        max_guests: rand(2...10),
+        address_line1: Faker::Address.street_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        postal_code: Faker::Address.zip_code(state_abbreviation: "CA"),
+        country: "United States",
+        status: Listing.statuses.to_a.sample[0],
+        latitude: Faker::Address.latitude,
+        longitude: Faker::Address.longitude,
+      )
+    # Each listing has 1 to 4 rooms
+    rand(1...4).times do
+      # Create room
+      room = listing.rooms.create(room_type: rand(0...3))
+
+      # Each room has 1 to 3 beds
+      rand(1...3).times { room.beds.create(bed_size: rand(0...5)) }
+    end
+  end
+end
+
 if user.nil?
   user = User.new(email: "b@b.com", password: "manager", password_confirmation: "manager")
   user.skip_confirmation!
   user.save!
 end
 
-10.times do
-  # Create listing
-  listing =
-    user.listings.create(
-      title: ("House " + Faker::Science.modifier + " " + Faker::Science.element),
-      about: Faker::Quote.jack_handey,
-      max_guests: rand(2...10),
-      address_line1: Faker::Address.street_address,
-      city: Faker::Address.city,
-      state: Faker::Address.state,
-      postal_code: Faker::Address.zip_code(state_abbreviation: "CA"),
-      country: "United States",
-      status: Listing.statuses.to_a.sample[0],
-      latitude: Faker::Address.latitude,
-      longitude: Faker::Address.longitude,
-    )
-  # Each listing has 1 to 4 rooms
-  rand(1...4).times do
-    # Create room
-    room = listing.rooms.create(room_type: rand(0...3))
+create_listings(user)
 
-    # Each room has 1 to 3 beds
-    rand(1...3).times { room.beds.create(bed_size: rand(0...5)) }
-  end
+5.times do
+  p = Faker::Internet.base64
+  user = User.new(email: Faker::Internet.email, password: p, encrypted_password: p, full_name: Faker::Name.name, avatar_url: "https://loremflickr.com/320/240")
+  user.skip_confirmation!
+  user.save!
+
+  create_listings(user)
 end
