ccf1631e2422f441220c990f1abc9cc8893034f8
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-25 23:32:18 -0800

    displaying photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,5 +10,10 @@
 #
 class Photo < ApplicationRecord
   belongs_to :listing
-  has_one_attached :photo
+  has_one_attached :photo do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :host_listings_thumb, resize_to_limit: [320, 320]
+  end
+
+  validates :caption, presence: true
 end
